58968ee640ebb6293c6523ebb7207f3d0cb27784
jordan_yim <jordanyim1@gmail.com>
2023-07-04 23:32:23 +1000

    no message

--- projects/space-takers/workspace/bullet.py
+++ projects/space-takers/workspace/bullet.py
@@ -7,4 +7,7 @@ class Bullet(pygame.sprite.Sprite):
         pygame.draw.circle(self.image, (255, 255, 0), (5, 5), 5)
         self.rect = self.image.get_rect(center=player.rect.center)
 
-    # ... rest of the class remains the same ...
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > 800:
+            self.kill()
--- projects/space-takers/workspace/enemy.py
+++ projects/space-takers/workspace/enemy.py
@@ -7,4 +7,7 @@ class Enemy(pygame.sprite.Sprite):
         pygame.draw.rect(self.image, (255, 0, 0), (0, 0, 50, 50))
         self.rect = self.image.get_rect(topleft=(800, 300))
 
-    # ... rest of the class remains the same ...
+    def update(self):
+        self.rect.x -= 3
+        if self.rect.right < 0:
+            self.kill()
