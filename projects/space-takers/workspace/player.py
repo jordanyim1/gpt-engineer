1e63f2c628be62bdb4aeb6ff62a78fdb27835361
jordan_yim <jordanyim1@gmail.com>
2023-07-04 23:41:44 +1000

    Put changes back

--- projects/space-takers/workspace/player.py
+++ projects/space-takers/workspace/player.py
@@ -12,10 +12,15 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= 5
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += 5
         if keys[pygame.K_UP]:
             self.rect.y -= 5
         if keys[pygame.K_DOWN]:
             self.rect.y += 5
+        self.bullets.update()
         if keys[pygame.K_SPACE]:
             self.shoot()
 
